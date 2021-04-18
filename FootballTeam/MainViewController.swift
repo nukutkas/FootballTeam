d551bdfc1f685dd0d15a5e163a4dc7e21d1f702b
Nikita Kochetov <nothsmple@mail.ru>
2021-04-18 18:32:23 +0300

    Row Height

--- FootballTeam/MainViewController.swift
+++ FootballTeam/MainViewController.swift
@@ -30,10 +30,17 @@ class MainViewController: UITableViewController {
         
         cell.textLabel?.text = footballclubNames[indexPath.row]
         cell.imageView?.image = UIImage(named: footballclubNames[indexPath.row])
+        cell.imageView?.layer.cornerRadius = cell.frame.size.height / 2
+        cell.imageView?.clipsToBounds = true
 
         return cell
     }
    
+    // MARK: - Table view delegate
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 85
+    }
     /*
     // MARK: - Navigation
 
