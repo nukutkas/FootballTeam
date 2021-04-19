1f7f993cfea2bb8790de29799efc0001fb4e1e08
Nikita Kochetov <nothsmple@mail.ru>
2021-04-19 23:39:29 +0300

    cell outlets

--- FootballTeam/CustomTableViewCell.swift
+++ FootballTeam/CustomTableViewCell.swift
@@ -8,8 +8,12 @@
 import UIKit
 
 class CustomTableViewCell: UITableViewCell {
+    
     @IBOutlet var imageOfClub: UIImageView!
     
-
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+    @IBOutlet var foundationYearLabel: UILabel!
+    
 
 }
